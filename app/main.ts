c47fd6f849adaa785b74d8887d4b13529066f5ae
DeronLee <akagilnc@gmail.com>
2016-05-20 22:57:36 +0800

    fix environment

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,4 @@
+///<reference path="./../typings/browser/ambient/es6-shim/index.d.ts"/>
 import { bootstrap }            from 'angular2/platform/browser';
 import { ROUTER_PROVIDERS }     from 'angular2/router';
 import { AppComponent }         from './app.component';
